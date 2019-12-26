47c87211d9188a8a332f3b753967ecb3692403eb
akbararyo <akbararyo@gmail.com>
2019-12-26 12:29:59 +0700

    Add files via upload

---
+++ BookCatalog/src/BookCatalog/Controller/BukuController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BookCatalog.Model.Entity;
+using BookCatalog.Model.Repository;
+
+namespace BookCatalog.Controller
+{
+    public class BukuController
+    {
+        private BukuRestApiRepository _repository;
+
+        public BukuController()
+        {
+            _repository = new BukuRestApiRepository();
+        }
+
+        public List<Buku> ReadByJudul(string judulBuku)
+        {
+            return _repository.ReadByJudul(judulBuku);
+        }
+
+        public List<Buku> ReadByPenerbit(string namaPenerbit)
+        {
+            return _repository.ReadByPenerbit(namaPenerbit);
+        }
+
+        public List<Buku> ReadByPengarang(string namaPengarang)
+        {
+            return _repository.ReadByPengarang(namaPengarang);
+        }
+    }
+}
---
+++ BookCatalog/src/BookCatalog/Model/Entity/Buku.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCatalog.Model.Entity
+{
+    public class Buku
+    {        
+        public string Isbn { get; set; }
+        public string Judul { get; set; }
+        public int Stok { get; set; }
+        public string Penerbit { get; set; }
+        public string Pengarang { get; set; }
+    }
+}
